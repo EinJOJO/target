8f1e6eef11c613f310a437b961cea6a01e7a86bf
Johannes / EinJojo <62117829+EinJOJO@users.noreply.github.com>
2022-04-07 07:29:50 +0200

    Initial commit

---
+++ src/main/java/de/einjojo/target/Target.java
@@ -0,0 +1,17 @@
+package de.einjojo.target;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Target extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
